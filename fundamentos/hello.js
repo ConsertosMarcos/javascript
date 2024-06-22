8d499ed93dbc7bf11a47c51269ded71bf0487b67
Marcos Almeida <tubaraogalatico2509@gmail.com>
2024-06-21 21:08:02 -0300

    fundamentos

---
+++ fundamentos/hello.js
@@ -0,0 +1,7 @@
+/**
+ * @author Marcos Ryan
+ * @link https://www.youtube.com/channel/UCiVuxdmrUV0yysFFzuHoOYg
+ */
+
+console.clear("Boa Noite Alunos") //limpar a tela.
+console.log("Boa Noite Alunos") // escrever na tele.
---
+++ fundamentos/operadores.js
@@ -0,0 +1,43 @@
+/**
+ * /**
+ * @author Marcos Ryan
+ * @link https://www.youtube.com/channel/UCiVuxdmrUV0yysFFzuHoOYg
+ * 
+ * Operadores
+    - Aritméticos
+    - Aritméticos de Atribuição
+    - Comparação
+    - Logicos
+ * @author Marcos Ryan
+ */
+
+console.clear()
+console.log("Operadores Aritmeticos")
+console.log("* | Multiplição")
+console.log("/ | Divisão")
+console.log("+ | Soma")
+console.log("- | Subitração")
+console.log("% | Módulo(sobra da divisão)")
+console.log("")
+console.log("Operadores Aritmeticos de Atribuição")
+console.log("*= | Multiplição Atribuição")
+console.log("/= | Divisão e Atribuição")
+console.log("+= | Soma e Atribuição")
+console.log("-= | Subitração e Atribuição")
+console.log("++ | Somar 1")
+console.log("-- | Subtrair 1")
+console.log("")
+console.log("Operadores Comparação")
+console.log(">   | Maior que")
+console.log(">=  | Maior ou igual a")
+console.log("<   | Menor que")
+console.log("<=  | Menor ou igual a")
+console.log("==  | Igual a")
+console.log("!=  | Diferente de")
+console.log("=== |Identico a")
+console.log("!== | Nao Identico a")
+console.log("")
+console.log("Operadores Logicos")
+console.log("&& |AND ")
+console.log("|| | OR ")
+console.log("!  | NOT ")
