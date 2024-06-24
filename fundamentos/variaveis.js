55ff4f82d1acd1e00ef0de359212aceff0c86973
Marcos Almeida <tubaraogalatico2509@gmail.com>
2024-06-24 19:51:09 -0300

    fundamentos

--- fundamentos/variaveis.js
+++ fundamentos/variaveis.js
@@ -2,3 +2,24 @@
  * @author Marcos Ryan
  * @link https://www.youtube.com/channel/UCiVuxdmrUV0yysFFzuHoOYg
  */
+
+console.clear()
+console.log("Constante___________")
+const nome = "Marcos Ryan"
+console.log(typeof(nome))
+console.log(nome)
+//nome = "Marcos Almeida"
+console.log("let_____________")
+let linguagem = "JAVA"
+console.log(linguagem)
+linguagem = "javaScript"
+console.log(linguagem)
+//let linguagem = "Python"
+//console.log(linguagem)
+console.log("var_____________")
+var time = "São Paulo"
+console.log(time)
+time = "Palmeira"
+console.log(log)
+var time = "Corinthians"
+console.log(time)
