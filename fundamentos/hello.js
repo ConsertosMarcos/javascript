20311b5f11f716e7602356c5ec15114c32f0e885
Marcos Almeida <tubaraogalatico2509@gmail.com>
2024-06-24 21:46:15 -0300

    javascript

--- fundamentos/hello.js
+++ fundamentos/hello.js
@@ -2,6 +2,7 @@
  * @author Marcos Ryan
  * @link https://www.youtube.com/channel/UCiVuxdmrUV0yysFFzuHoOYg
  */
+const colors = require('colors')
 
 console.clear("Boa Noite Alunos") //limpar a tela.
-console.log("Boa Noite Alunos") // escrever na tele.
+console.log("Boa Noite Alunos".rainbow) // escrever na tele.
