e80d3a83a97edfe6bba39d7e1650122f80a067ad
Marcos Almeida <tubaraogalatico2509@gmail.com>
2024-06-24 22:09:20 -0300

    javascript

---
+++ exercicios/media.js
@@ -0,0 +1,22 @@
+/**
+ * Calculo da media - de duas notas 
+ * @author Marcos Ryan
+ * @link https://www.youtube.com/channel/UCiVuxdmrUV0yysFFzuHoOYg
+ */
+
+//importar a biblioteca
+const imput = require('readline-sync')
+
+//variaveis
+let nota1, nota2, media
+
+console.clear()
+console.log ("Calculo da Media")
+//captura da nota pelo pacote readline-sync
+
+//entrada
+nota1 = Number(imput.question("digite a nota1: ")) 
+//console.log (nota1)
+nota2 =Number(imput.question("digite a nota2: ")) 
+//ConstantSourceNode.log (nota2)
+media 
