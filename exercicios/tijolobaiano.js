14e81a2a6fc35605734995c307847f0a79207e29
Marcos Almeida <tubaraogalatico2509@gmail.com>
2024-07-02 21:32:14 -0300

    javascript

---
+++ exercicios/tijolobaiano.js
@@ -0,0 +1,19 @@
+/**
+ * Calculo da quantidade de tijolos baiano 8 furos por metro quadrado
+ * @author Marcos Ryan
+ * @link https://www.youtube.com/channel/UCiVuxdmrUV0yysFFzuHoOYg
+ */
+
+const imput = require('readline-sync')
+
+//variaveis
+let m2, tijolos
+
+console.log()
+console.log("calculo de tijolos baiano por metro quadrado")
+
+//entrada
+m2 = Number(imput.question("quantos metros voce precisa?"))
+
+tijolos = m2 * 28
+console.log(`voce precisara de ${tijolos} tijolos`)
