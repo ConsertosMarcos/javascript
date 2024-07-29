9aa0164c1dbf9b51a48d13f085f965273c3f9157
Marcos Almeida <tubaraogalatico2509@gmail.com>
2024-07-29 20:08:12 -0300

    javascript

---
+++ exercicios/flex.js
@@ -0,0 +1,15 @@
+/**
+ * Calculo Gasolina ou Etanol
+ * @author Marcos Ryan
+ * @link https://www.youtube.com/channel/UCiVuxdmrUV0yysFFzuHoOYg
+ */
+
+//importar a biblioteca
+const imput = require('readline-sync')
+
+//variaveis
+let Gasolina, Etanol
+ 
+console.clear ()
+console.log (Gasolina ou Etanol)
+
