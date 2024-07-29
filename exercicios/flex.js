1b40987f48a31047e9d44a1912d8d57e9b6d57a0
Marcos Almeida <tubaraogalatico2509@gmail.com>
2024-07-29 20:17:32 -0300

    javascript

--- exercicios/flex.js
+++ exercicios/flex.js
@@ -4,12 +4,23 @@
  * @link https://www.youtube.com/channel/UCiVuxdmrUV0yysFFzuHoOYg
  */
 
-//importar a biblioteca
-const imput = require('readline-sync')
+//importar a biblioteca/pacotes
+const imput = require('colors')
+const input = require('readline-sync')
 
 //variaveis
 let Gasolina, Etanol
  
 console.clear ()
-console.log (Gasolina ou Etanol)
+console.log (Etanol ou Gasolina)
 
+//entrada
+Etanol = Number(input.question("Digite o valor do Etanol"))
+Gasolina = Number(input.question("Digite o valor da Gasolina"))
+
+//Processamento/saída
+if (Etanol < 0.7 * Gasolina) {
+    console.log("Abastecer com Etanol".green)
+} else {
+    console.log("Abastecer com Gasolina".yellow)
+}
