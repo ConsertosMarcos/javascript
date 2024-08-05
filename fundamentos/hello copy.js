79bc2155c9680a89d6d82258e9e3024ed8a7f8c3
Marcos Almeida <tubaraogalatico2509@gmail.com>
2024-08-05 19:25:09 -0300

    javascript

---
+++ fundamentos/hello copy.js
@@ -0,0 +1,8 @@
+/**
+ * @author Marcos Ryan
+ * @link https://www.youtube.com/channel/UCiVuxdmrUV0yysFFzuHoOYg
+ */
+const colors = require('colors')
+
+console.clear("Boa Noite Alunos") //limpar a tela.
+console.log("Boa Noite Alunos".rainbow) // escrever na tele.
---
+++ fundamentos/sintaxe1.js
@@ -0,0 +1,14 @@
+/**
+ * Sintaxe moderna nas estruturas if if-else
+ * @author Marcos Ryan
+ * @link https://www.youtube.com/channel/UCiVuxdmrUV0yysFFzuHoOYg
+ */
+
+let media = 4
+
+// na linguagem java script podemos omitir o uso de chaves na estrutura if if-else porem apenas uma linha de codigo é permitida
+
+if (media < 5) 
+    console.log("Reprovado")
+ else 
+    console.log("Aprovado")
