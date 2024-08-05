ff1c2e9ed0aa43ee386a50d72c99b8d2553850cb
Marcos Almeida <tubaraogalatico2509@gmail.com>
2024-08-05 19:50:05 -0300

    javascript

--- exercicios/media.js
+++ exercicios/media.js
@@ -6,6 +6,7 @@
 
 //importar a biblioteca
 const imput = require('readline-sync')
+const colors = require('colors')
 
 //variaveis
 let nota1, nota2, media
@@ -15,12 +16,20 @@ console.log ("Calculo da Media")
 //captura da nota pelo pacote readline-sync
 
 //entrada:
-nota1 = Number(imput.question("digite a nota1: ")) 
+nota1 = Number(imput.question("digite a nota1:")) 
 //console.log (nota1)
-nota2 =Number(imput.question("digite a nota2: ")) 
+nota2 =Number(imput.question("digite a nota2:")) 
 //console.log (nota2)
 
 //processamento:
 media = (nota1, nota2) / 2
 //saida
 console.log (`Média: ${media.toFixed(1)}`)
+//exemplo de uso de estrutura  if - else if
+if (media < 2) {
+    console.log("REPROVADO".red)
+} else if (media >= 2 && media < 5) {
+    console.log("RECUPERAÇÃO".yellow)
+} else {
+    console.log("APROVADO".blue)
+}
