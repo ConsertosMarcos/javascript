0e7cd3933446794e3b50cdd3b6dbb57416dbec8d
Marcos Almeida <tubaraogalatico2509@gmail.com>
2024-08-07 20:45:02 -0300

    javascript

---
+++ exercicios/jogo.js
@@ -0,0 +1,43 @@
+/**
+ * Tigrinho do Senac
+ * @author Marcos Ryan
+ * @link https://www.youtube.com/channel/UCiVuxdmrUV0yysFFzuHoOYg
+ */
+
+const input = require('readline-sync')
+const colors = require('colors')
+
+//variaveis
+let face
+
+console.clear()
+console.log("Sorteo de Dados".red.bold)
+input.question("Pressione a tecla [ENTER] para jogar o DADO: ")
+
+//sorteio da face de um dado
+//math(classe matematica)
+//floor(conversao para numeros inteiros)
+//gandom() * 6 (gerador de numeros aleatorios 0 1 2 3 4 5)
+face = Math.floor(Math.random() * 6 + 1)
+
+console.log(`Face do dado: ${face}`.blue)
+switch (face) {
+    case 1:
+        console.log("⚀")
+        break
+    case 2:
+        console.log("⚁")
+        break
+    case 3:
+        console.log("⚂")
+        break
+    case 4:
+        console.log("⚃")
+        break
+    case 5:
+        console.log("⚄")
+        break
+    case 6:
+        console.log("⚅")
+        break
+}
