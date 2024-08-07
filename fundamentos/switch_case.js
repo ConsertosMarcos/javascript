14e62ec20f17780d25767da5c50a891a8fd246ea
Marcos Almeida <tubaraogalatico2509@gmail.com>
2024-08-07 19:53:46 -0300

    javascript

---
+++ fundamentos/switch_case.js
@@ -0,0 +1,28 @@
+/**
+ * Estudo de estrutura Switch-Case
+ * @author Marcos Ryan
+ * @link https://www.youtube.com/channel/UCiVuxdmrUV0yysFFzuHoOYg
+ */
+
+const input = require('readline-sync')
+
+let opcao
+
+console.clear()
+
+console.log("Boot - Escolha o sistema operacional")
+console.log("1. Windows")
+console.log("2. Linux")
+opcao = Number(input.question("Digite a opcao desejada:"))
+switch (opcao) {
+    case 1:
+        console.clear()
+        console.log("Carregando o Windows...............................")
+        break
+    case 2:
+        console.clear()
+        console.log("Carregandi Linux........")
+        break
+    default:
+        console.log("Opção invalida")
+}
