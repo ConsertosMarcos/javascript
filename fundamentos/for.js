3c6fbacfdefece49f4e28c92e17c9bafeb2840b2
Marcos Almeida <tubaraogalatico2509@gmail.com>
2024-08-09 19:57:30 -0300

    javascript

---
+++ fundamentos/for.js
@@ -0,0 +1,15 @@
+/**
+ * Exemplo da estrutura for
+ * @author Marcos Ryan
+ * @link https://www.youtube.com/channel/UCiVuxdmrUV0yysFFzuHoOYg
+ */
+
+console.clear()
+for (let i = 1; i < 11; i++) {
+    console.log(i)
+}
+
+console.log("")
+for (let i = 10; i > 0; i--) {
+    console.log(i)
+}
