b938146f0606c51649ff0b1b135c28f7e45dd303
Marcos Almeida <tubaraogalatico2509@gmail.com>
2024-08-14 19:46:36 -0300

    javascript

--- fundamentos/while.js
+++ fundamentos/while.js
@@ -1,26 +1,26 @@
 /**
- * Estudo das estruturas while e do-while
+ * Exemplo da estrutura while do-while
  * @author Marcos Ryan
  * @link https://www.youtube.com/channel/UCiVuxdmrUV0yysFFzuHoOYg
  */
 
-console.clear();
+const input = require('readline-sync')
 
-// Importar o pacote readline-sync (input no console)
-const input = require('readline-sync');
+//modificar valores da variavel para ver qual a diferença das estruturas
+let x = 1
+let y = 1
 
-// Modificar os valores para testar a diferença entre while e do-while
-let x = 1;
-let y = 1;
+console.clear()
+
 
 while (x < 11) {
-    console.log("E");
+    console.log("teste da estrutura whitle")
     x++
 }
 
-input.question("Pressione a tecla [Enter] para continuar");
-console.clear();
+input.question("Pressione a tecla [ENTER] para continuar: ")
+console.clear()
 do {
-    console.log("Teste da estrutura do-while");
+    console.log("teste da do-estrutura do-whitle")
     y++
-} while (y < 11);
+} while (y < 11)
