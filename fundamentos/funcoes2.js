ec1cdd2498d4f343bac5fef4131debf95f6bf5fa
Marcos Almeida <tubaraogalatico2509@gmail.com>
2024-08-14 20:53:41 -0300

    fundamentos

--- fundamentos/funcoes2.js
+++ fundamentos/funcoes2.js
@@ -3,3 +3,31 @@
  * @author Marcos Ryan
  * @link https://www.youtube.com/channel/UCiVuxdmrUV0yysFFzuHoOYg
  */
+
+//IMORTANTE !!! Funçoes atribuidas obrigatoriamente precisam ser criadas no inicio do codigo (antes de serem execultadas)
+
+function somar(num1, num2) {
+    return (console.log(num1 + num2))
+}
+
+somar(2, 5)
+
+//funçao atribuida (funçao anonima) com parametros
+let somarAtribuida = function (num1, num2) {
+    return (console.log(num1 + num2))
+}
+
+somarAtribuida(10, 6)
+
+//funçao atribuida simplificada(arrow function)  com parametros
+
+let somarAF = (num1, num2) => {
+    return (console.log(num1 + num2))
+}
+
+somarAF(8, 5)
+
+//funçao atribuida simplificada (arrow function) com parametros 1 linha de codigo (besre caso o return é implicito)
+let somarAFS = (num1, num2) => console.log(num1 + num2)
+
+somarAFS(9, 5)
