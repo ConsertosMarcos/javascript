3b514cc91ffe8fd8b8b7f167e8a5136fee80f2cc
Marcos Almeida <tubaraogalatico2509@gmail.com>
2024-08-14 22:10:55 -0300

    exercicios

--- exercicios/calculadora.js
+++ exercicios/calculadora.js
@@ -9,16 +9,52 @@ const input = require('readline-sync')
 const colors = require('colors')
 
 //Variaveis
+let num1, num2, operacao
 
-//Console
-console.clear()
-console.log(" _____   ___   _     _____ _   _ _       ___ ______ ___________  ___  ".red.bold)
-console.log("/  __ \\ / _ \\ | |   /  __ \\ | | | |     / _ \\|  _  \\  _  | ___ \\/ _ \\ ".red.bold)
-console.log("| /  \\// /_\\ \\| |   | /  \\/ | | | |    / /_\\ \\ | | | | | | |_/ / /_\\ \\".red.bold)
-console.log("| |    |  _  || |   | |   | | | | |    |  _  | | | | | | |    /|  _  |".red.bold)
-console.log("| \\__/\\| | | || |___| \\__/\\ |_| | |____| | | | |/ /\\ \\_/ / |\\ \\| | | |".red.bold)
-console.log(" \\____/\\_| |_/\\_____/\\____/\\___/\\_____/\\_| |_/___/  \\___/\\_| \\_\\_| |_/".red.bold)
+//variavel funcao
+let somarAFS = (num1, num2) => console.log(`${num1} + ${num2} = ${num1 + num2}`)
+let SubtrairAFS = (num1, num2) => console.log(`${num1} - ${num2} = ${num1 - num2}`)
+let MultiplicarAFS = (num1, num2) => console.log(`${num1} x ${num2} = ${num1 * num2}`)
+let DividirAFS = (num1, num2) => console.log(`${num1} / ${num2} = ${num1 / num2}`)
+let PorcentagemAFS = (num1, num2) => console.log(`${num1} % ${num2} = ${(num1 * num2) / 100}`)
 
-//entrada da calculadora
-console.log("")
-console.log("")
+do {
+    console.clear()
+    console.log(" _____   ___   _     _____ _   _ _       ___ ______ ___________  ___  ".red.bold)
+    console.log("/  __ \\ / _ \\ | |   /  __ \\ | | | |     / _ \\|  _  \\  _  | ___ \\/ _ \\ ".red.bold)
+    console.log("| /  \\// /_\\ \\| |   | /  \\/ | | | |    / /_\\ \\ | | | | | | |_/ / /_\\ \\".red.bold)
+    console.log("| |    |  _  || |   | |   | | | | |    |  _  | | | | | | |    /|  _  |".red.bold)
+    console.log("| \\__/\\| | | || |___| \\__/\\ |_| | |____| | | | |/ /\\ \\_/ / |\\ \\| | | |".red.bold)
+    console.log(" \\____/\\_| |_/\\_____/\\____/\\___/\\_____/\\_| |_/___/  \\___/\\_| \\_\\_| |_/".red.bold)
+
+    //entrada
+    num1 = Number(input.question('Digite o primeiro numero: '))
+    num2 = Number(input.question('Digite o segundo numero: '))
+    console.log("")
+    console.log("1. Somar")
+    console.log("2. Subtrair")
+    console.log("3. Multiplicar")
+    console.log("4. Dividir")
+    console.log("5. Calcular porcentagem")
+    operacao = Number(input.question("Digite a opcao desejada: "))
+
+
+    switch (operacao) {
+        case 1:
+            somarAFS(num1, num2)
+            break
+        case 2:
+            SubtrairAFS(num1, num2)
+            break
+        case 3:
+            MultiplicarAFS(num1, num2)
+            break
+        case 4:
+            DividirAFS(num1, num2)
+            break
+        case 5:
+            PorcentagemAFS(num1, num2)
+            break
+    }
+    confirma = input.question("Deseja Calcular Novamente(s/n): ")
+} while (confirma === "s")
