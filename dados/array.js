8108c4f38406ef7c30c3ea66b06e3eeb7ef44676
Marcos ryan <tubaraogalatico2509@gmail.com>
2024-08-23 19:35:50 -0300

    javascript

---
+++ dados/array.js
@@ -0,0 +1,9 @@
+/**
+ * Estrutura de Dados
+ * @author Marcos Ryan
+ * @link https://www.youtube.com/channel/UCiVuxdmrUV0yysFFzuHoOYg
+ */
+
+//array simples
+let times =[]
+console.log(typeof(times))
