2f806302366af1137012a87c565347505832a89d
Marcos ryan <tubaraogalatico2509@gmail.com>
2024-08-23 20:01:19 -0300

    javascript

--- dados/array.js
+++ dados/array.js
@@ -5,5 +5,20 @@
  */
 
 //array simples
-let times =[]
-console.log(typeof(times))
+//             [0]             [1]         [2]         [3]
+let times = ["Corinthians", "São Paulo", "Santos", "Palmeiras"]
+//console.log(typeof (times))
+console.log(times)
+console.table(times)
+console.log(times[0])
+
+
+//array multidimensional (matriz)
+
+let boletim = [[8, 7, 9, 3], [4, 5, 8, 6]]//"linhas de uma tabela"
+console.log(boletim)
+console.table(boletim)
+console.log(boletim[0][1])//[linha][coluna]
+
+
+//array 
