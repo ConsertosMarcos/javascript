ee9903f7a08721e37c3cf1a4cc1f54eae39887b4
Marcos ryan <tubaraogalatico2509@gmail.com>
2024-09-02 22:09:19 -0300

    javascript

---
+++ poo/media/app.js
@@ -0,0 +1,14 @@
+/**
+ * Calculo da Média
+ * @author Marcos Ryan
+ * @link https://www.youtube.com/channel/UCiVuxdmrUV0yysFFzuHoOYg
+ */
+
+//variaveis
+let nota1, nota2, media
+
+function calcular() {
+    //entrada
+    nota1 = Number(frmMedia.txtNota1.value)
+    nota2 = Number(frmMedia.txtNota2.value)
+}
